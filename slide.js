48abec5d5b15492d833b9c84281be4b2623fb41c
Gregoire Bessagnet <gregoire.bessagnet@inbox.com>
2017-03-26 18:06:43 +0200

    added openclassromm solution

--- slide.js
+++ slide.js
@@ -1,43 +1,75 @@
 $(document).ready(function(){
 
-    var sliderimages = $('ul li img');
-	var slidersize = sliderimages.width()
+var images = ['img/img1.jpg', 'img/img2.jpg', 'img/img3.jpg', 'img/img4.jpg', 'img/img5.jpg', 'img/img6.jpg'];
 
-	$('ul').wrap('<div id="slider" />');
-
-	$('#slider').css({
-		width: function() {
-		return slidersize;
-	  },
-		height: function() {
-		return sliderimages.height();
-	  },
-		position: 'relative',
-		overflow: 'hidden'
-	});
-
-	var totalsize = sliderimages.length * slidersize;
-	$('ul').css({
-		width: function(){
-		return totalsize;
-	}
+jQuery.each( images, function(i, val) {
+		$("#slider").append("<img src=" + val + ">")
 });
 
-$(sliderimages).each(
-  function(intIndex){
-    $(this).nextAll('a')
-  	.bind("click", function(){
-  		if($(this).is(".next"))	{
-  			$(this).parent('li').parent('ul').animate({
-  				"margin-left": (-(intIndex + 1) * slidersize)
-  					}, 1000)
-  			} else if($(this).is(".previous")){
-  			$(this).parent('li').parent('ul').animate({
-  				"margin-left": (-(intIndex - 1) * slidersize)
-  			}, 1000)
-  			}
-  	})
-	});
+var $carrousel = $('#slider'), 
+    $img = $('#slider img'), 
+    indexImg = $img.length - 1, 
+    i = 0, 
+    $currentImg = $img.eq(i);
+
+$img.css('display', 'none'); 
+$currentImg.css('display', 'block'); 
+
+$carrousel.wrap('<div class="controls"> <span class="prev  glyphicon glyphicon-circle-arrow-left"></span> <span class="next glyphicon glyphicon-circle-arrow-right"></span> </div>');
+
+$('.next').click(function(){ 
+
+    i++; 
+
+    if( i <= indexImg ){
+        $img.css('display', 'none'); 
+        $currentImg = $img.eq(i); 
+        $currentImg.css('display', 'block');
+    }
+    else{
+        i = indexImg;
+    }
+
+});
+
+$('.prev').click(function(){
+
+    i--; 
+
+    if( i >= 0 ){
+        $img.css('display', 'none');
+        $currentImg = $img.eq(i);
+        $currentImg.css('display', 'block');
+    }
+    else{
+        i = 0;
+    }
+
+});
+
+
+function slideImg(){
+    setTimeout(function(){ 
+						
+        if (i < indexImg){ 
+	    	i++; 
+	}
+	else { 
+	    i = 0;
+	}
+
+	$img.css('display', 'none');
+
+	$currentImg = $img.eq(i);
+	$currentImg.css('display', 'block');
+
+	slideImg();
+
+    }, 3000);
+}
+
+slideImg(); 
+
 });
 
 
